def14923bc6f11c8a6989eee5af5398ead7c41ca
Chohanwool <hanwoolc95@gmail.com>
2025-07-30 20:22:44 +0900

    [chore] responsive_helper.dart 주석 추가

--- lib/utils/responsive_helper.dart
+++ lib/utils/responsive_helper.dart
@@ -1,6 +1,14 @@
 
 import 'package:flutter/material.dart';
 
+/// Helper 클래스
+/// - 화면 크기에 따라 UI 요소(폰트, 여백, 아이콘 등)의 크기를 동적으로 계산해주는 클래스
+/// - (before) 헬퍼가 없었다면 모든 위젯 파일마다 MediaQuery.of(context).size.height / 1366 * 40  와 같은 계산 코드를 직접 사용해야함
+/// - (after) 헬퍼를 통해 context.fsp(40) 과 같이 직관적이고 짧은 코드로 반응형 크기를 얻을 수 있음
+///
+/// - Extension 메서드 사용
+///   - Dart에서 제공하는 기능으로, 기존 클래스에 새로운 기능을 확장하는 것처럼 추가
+
 extension ResponsiveHelper on BuildContext {
   // 기준이 되는 기기의 너비와 높이 (예: iPad Pro 12.9인치)
   static const double _baseWidth = 1024;
