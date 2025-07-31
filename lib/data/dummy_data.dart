32ab25d45badb4bfaabd5d2c196e78a932dddf4b
Chohanwool <hanwoolc95@gmail.com>
2025-07-31 18:35:01 +0900

    [feat] DB 연동 전 더미 데이터 추가

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,14 @@
+import 'package:catch_table/models/registration.dart';
+
+final List<Registration> dummyRegistrations = [
+  Registration(phoneNumber: '09171234567', groupSize: 2),
+  Registration(phoneNumber: '9171234567', groupSize: 4),
+  Registration(phoneNumber: '+639181234567', groupSize: 3),
+  Registration(phoneNumber: '09281234567', groupSize: 1),
+  Registration(phoneNumber: '9391234567', groupSize: 6),
+  Registration(phoneNumber: '+639991234567', groupSize: 5),
+  Registration(phoneNumber: '09981234567', groupSize: 8),
+  Registration(phoneNumber: '09051234567', groupSize: 10),
+  Registration(phoneNumber: '09391234567', groupSize: 7),
+  Registration(phoneNumber: '09561234567', groupSize: 9),
+];
